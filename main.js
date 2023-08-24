54c3ac7e21b24977a11468b98415d9807872a328
marvin-garcia-yepes <76126340+marvin-garcia-yepes@users.noreply.github.com>
2023-08-24 13:05:34 -0500

    funcionalidad

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const hamburgerMenu = document.querySelector('#hamburger-menu');
 const closeBtn = document.querySelector('#close-btn');
 const navbar = document.getElementById('navbar');
+const featuresBtn = document.querySelector('#features-btn');
+const companyBtn = document.querySelector('#company-btn');
+const featuresUl = document.querySelector('#features-ul');
+const companyUl = document.querySelector('#company-ul');
 
 hamburgerMenu.addEventListener('click', ()=>{
     navbar.style.display = 'block';
@@ -8,4 +12,20 @@ hamburgerMenu.addEventListener('click', ()=>{
 
 closeBtn.addEventListener('click', ()=>{
     navbar.style.display = 'none';
+});
+
+featuresBtn.addEventListener('click', ()=>{
+    if(featuresUl.style.display === 'block'){
+        featuresUl.style.display = 'none'
+    }else{
+        featuresUl.style.display = 'block'
+    }
+});
+
+companyBtn.addEventListener('click', ()=>{
+    if(companyUl.style.display === 'block'){
+        companyUl.style.display = 'none'
+    }else{
+        companyUl.style.display = 'block'
+    }
 });
